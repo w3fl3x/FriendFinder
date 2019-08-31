3093c93e1e1705202d48a4048a547bf2602c100c
Huy Nguyen <huy.m.nguyen@live.com>
2019-08-31 00:13:28 -0700

    Add friends to friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,34 @@
+var friends = [
+    {
+        name: 'Mickey',
+        photo: 'https://www.disneyclips.com/images/images/mickey-mouse-pointing-you.png',
+        scores: []
+    },
+    {
+        name: 'Minnie',
+        photo: 'https://www.disneyclips.com/images/images/minnie-secret.png',
+        scores: []
+    },
+    {
+        name: 'Donald',
+        photo: 'https://www.disneyclips.com/images/images/donald-duck12.png',
+        scores: []
+    },
+    {
+        name: 'Daisy',
+        photo: 'https://www.disneyclips.com/images/images/daisy_surprised.gif',
+        scores: []
+    },
+    {
+        name: 'Goofy',
+        photo: 'https://www.disneyclips.com/images/images/goofy-upside-down.png',
+        scores: []
+    },
+    {
+        name: 'Pluto',
+        photo: 'https://www.disneyclips.com/images/images/pluto-skateboarding.png',
+        scores: []
+    }
+];
+
+module.exports = friends;
