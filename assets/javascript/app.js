d5ca44671e17d619c32304ec5aaa8569d061f5d2
Huy Nguyen <huy.m.nguyen@live.com>
2019-08-31 01:13:50 -0700

    Add script for on click of submit button from survey.html. Add modal div to best match friend in survey.html

---
+++ assets/javascript/app.js
@@ -0,0 +1,41 @@
+$('#submitBtn').on('click', function(event) {
+    event.preventDefault();
+
+    var valid = true;
+
+    if($('#name').val() === '' || $('#photo').val() === '') {
+        valid = false;
+    } else if($('#q1').val() === 'empty' || $('#q2').val() === 'empty' || $('#q3').val() === 'empty' || $('#q4').val() === 'empty' || $('#q5').val() === 'empty' || $('#q6').val() === 'empty' || $('#q7').val() === 'empty' || $('#q8').val() === 'empty' || $('#q9').val() === 'empty' || $('#q10').val() === 'empty') {
+        valid = false;
+    }
+
+    if(valid === true) {
+        var newUser = {
+            name: $('#name').val().trim(),
+            photo: $('#photo').val().trim(),
+            scores: [
+                $('#q1').val(),
+                $('#q2').val(),
+                $('#q3').val(),
+                $('#q4').val(),
+                $('#q5').val(),
+                $('#q6').val(),
+                $('#q7').val(),
+                $('#q8').val(),
+                $('#q9').val(),
+                $('#q10').val()
+            ]
+        };
+
+        var currentURL = window.location.origin;
+
+        // AJAX call for response after post
+        $.post(currentURL + '/api/friends', newUser, function(data) {
+            $('#bestFriend').text(data.name);
+            $('#bestFriendPhoto').attr('src', data.photo);
+            $('#bestFriendModal').modal('toggle');
+        });
+    }else {
+        alert('Survey is incomplete!');
+    };
+});
