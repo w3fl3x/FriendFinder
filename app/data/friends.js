da4ae4f81575ab76d945b4e55def768ed314e20c
Huy Nguyen <huy.m.nguyen@live.com>
2019-09-02 01:21:15 -0700

    Add scores array for each friends in friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,32 +2,32 @@ var friends = [
     {
         name: 'Mickey',
         photo: 'https://www.disneyclips.com/images/images/mickey-mouse-pointing-you.png',
-        scores: []
+        scores: [5, 4, 5, 4, 5, 5, 2, 1, 5, 5]
     },
     {
         name: 'Minnie',
         photo: 'https://www.disneyclips.com/images/images/minnie-secret.png',
-        scores: []
+        scores: [4, 4, 5, 3, 5, 4, 2, 2, 4, 5]
     },
     {
         name: 'Donald',
         photo: 'https://www.disneyclips.com/images/images/donald-duck12.png',
-        scores: []
+        scores: [5, 2, 3, 4, 3, 3, 4, 5, 2, 3]
     },
     {
         name: 'Daisy',
         photo: 'https://www.disneyclips.com/images/images/daisy_surprised.gif',
-        scores: []
+        scores: [3, 4, 4, 3, 5, 3, 2, 2, 2, 4]
     },
     {
         name: 'Goofy',
         photo: 'https://www.disneyclips.com/images/images/goofy-upside-down.png',
-        scores: []
+        scores: [2, 1, 3, 4, 2, 4, 5, 5, 1, 2]
     },
     {
         name: 'Pluto',
         photo: 'https://www.disneyclips.com/images/images/pluto-skateboarding.png',
-        scores: []
+        scores: [1, 1, 1, 5, 1, 3, 1, 4, 1, 1]
     }
 ];
 
